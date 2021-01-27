1fd262677f3f50d8225c51c1d43c6324f2c0e83c
last_arg <last_arg@tutanota.com>
2021-01-27 20:38:52 +0200

    parse html page title also

--- src/find_rss.zig
+++ src/find_rss.zig
@@ -21,13 +21,20 @@ const l = std.log;
 
 const Tag = enum {
     none,
+    title,
     link_or_a,
 };
+
+const Page = struct {
+    title: ?[]const u8 = null,
+    rss_links: []Link,
+};
+
 const Link = struct {
     href: []const u8,
     title: ?[]const u8 = null,
 };
-pub fn findFeedLinks(allocator: *Allocator, contents: []const u8) ![]Link {
+pub fn findFeedLinks(allocator: *Allocator, contents: []const u8) !Page {
     // Remove first brackets from html '<!doctype html>'
     var start_index = mem.indexOfScalar(u8, contents, '>') orelse return error.InvalidHtml;
     start_index += 1;
@@ -37,6 +44,8 @@ pub fn findFeedLinks(allocator: *Allocator, contents: []const u8) ![]Link {
 
     var links = ArrayList(Link).init(allocator);
     defer links.deinit();
+
+    var page_title: ?[]const u8 = null;
 
     var link_rel: ?[]const u8 = null;
     var link_type: ?[]const u8 = null;
@@ -49,82 +58,95 @@ pub fn findFeedLinks(allocator: *Allocator, contents: []const u8) ![]Link {
         // warn("{}\n", .{event});
         switch (event) {
             .open_tag => |tag| {
+                // warn("open_tag: {s}\n", .{tag});
                 if (mem.eql(u8, "link", tag) or mem.eql(u8, "a", tag)) {
                     active_tab = .link_or_a;
-                    warn("open_tag: {s}\n", .{tag});
+                } else if (mem.eql(u8, "title", tag)) {
+                    active_tab = .title;
                 }
             },
             .close_tag => |tag| {
                 // warn("close_tag: {s}\n", .{tag});
-                if (active_tab == .link_or_a) {
-                    const valid_rel = link_rel != null and mem.eql(u8, "alternate", link_rel.?);
-                    const valid_type = link_type != null and mem.eql(u8, "application/rss+xml", link_type.?);
-                    if (valid_rel and valid_type) {
-                        if (link_href) |href| {
-                            var is_duplicate = blk: {
-                                for (links.items) |link| {
-                                    if (mem.eql(u8, link.href, href)) {
-                                        break :blk true;
+                switch (active_tab) {
+                    .link_or_a => {
+                        const valid_rel = link_rel != null and mem.eql(u8, "alternate", link_rel.?);
+                        const valid_type = link_type != null and mem.eql(u8, "application/rss+xml", link_type.?);
+                        if (valid_rel and valid_type) {
+                            if (link_href) |href| {
+                                var is_duplicate = blk: {
+                                    for (links.items) |link| {
+                                        if (mem.eql(u8, link.href, href)) {
+                                            break :blk true;
+                                        }
                                     }
+                                    break :blk false;
+                                };
+                                if (!is_duplicate) {
+                                    try links.append(Link{
+                                        .href = href,
+                                        .title = link_title,
+                                    });
                                 }
-                                break :blk false;
-                            };
-                            if (!is_duplicate) {
-                                try links.append(Link{
-                                    .href = href,
-                                    .title = link_title,
-                                });
                             }
                         }
-                    }
-                    link_rel = null;
-                    link_type = null;
-                    link_title = null;
-                    link_href = null;
-                    active_tab = .none;
+                        link_rel = null;
+                        link_type = null;
+                        link_title = null;
+                        link_href = null;
+                        active_tab = .none;
+                    },
+                    .title => {
+                        active_tab = .none;
+                    },
+                    .none => {},
                 }
             },
             .attribute => |attr| {
                 // warn("attribute\n", .{});
-                if (active_tab == .link_or_a) {
-                    warn("\tname: {s}\n", .{attr.name});
-                    warn("\traw_value: {s}\n", .{attr.raw_value});
-                    if (mem.eql(u8, "rel", attr.name)) {
-                        link_rel = attr.raw_value;
-                    } else if (mem.eql(u8, "type", attr.name)) {
-                        link_type = attr.raw_value;
-                    } else if (mem.eql(u8, "title", attr.name)) {
-                        link_title = attr.raw_value;
-                    } else if (mem.eql(u8, "href", attr.name)) {
-                        link_href = attr.raw_value;
-                    }
+                // warn("\tname: {s}\n", .{attr.name});
+                // warn("\traw_value: {s}\n", .{attr.raw_value});
+                switch (active_tab) {
+                    .link_or_a => {
+                        if (mem.eql(u8, "rel", attr.name)) {
+                            link_rel = attr.raw_value;
+                        } else if (mem.eql(u8, "type", attr.name)) {
+                            link_type = attr.raw_value;
+                        } else if (mem.eql(u8, "title", attr.name)) {
+                            link_title = attr.raw_value;
+                        } else if (mem.eql(u8, "href", attr.name)) {
+                            link_href = attr.raw_value;
+                        }
+                    },
+                    .none, .title => {},
                 }
             },
             .comment => |str| {
                 // warn("comment: {s}\n", .{str});
             },
             .processing_instruction => |str| {
-                warn("processing_instruction: {s}\n", .{str});
+                // warn("processing_instruction: {s}\n", .{str});
             },
             .character_data => |value| {
                 // warn("character_data: {s}\n", .{value});
+                if (active_tab == .title) {
+                    page_title = value;
+                }
             },
         }
     }
 
-    return links.toOwnedSlice();
+    return Page{
+        .title = page_title,
+        .rss_links = links.toOwnedSlice(),
+    };
 }
 
-pub fn isDuplicateHref(links: []Link, href: []const u8) bool {}
-
 test "parse html" {
+    const expect = testing.expect;
     var arena = std.heap.ArenaAllocator.init(testing.allocator);
     defer arena.deinit();
     const allocator = &arena.allocator;
     const html = @embedFile("../test/lobste.rs.html");
-    const links = try findFeedLinks(allocator, html);
-    // warn("len: {}\n", .{links.len});
-    for (links) |link| {
-        warn("link: {s}\n", .{link.href});
-    }
+    const page = try findFeedLinks(allocator, html);
+    expect(4 == page.rss_links.len);
 }
