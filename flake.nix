9c9b320387dd4722aeaa4159ee7fa39e86dcfd46
last-arg <last_arg@tutanota.com>
2023-01-15 16:42:24 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    zig.url = "github:roarkanize/zig-overlay";
+    zig.url = "github:mitchellh/zig-overlay";
   };
 
   outputs = { self, nixpkgs, zig, flake-utils }@inputs:
@@ -13,8 +13,8 @@
         let
           # pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}.master."2022-01-24"; };
           # Because std.math.round fn doesn't exist anymore (zig-dateformat package)
-          pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}.master."2022-04-15"; };
-          # pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}.master."latest"; };
+          # pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}.master."2022-04-15"; };
+          pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}.master; };
         in
         {
           devShell = import ./shell.nix { inherit pkgs; };
