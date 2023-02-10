f316a6a11f6dccb31748848af497cf7ddc93cb30
last-arg <last_arg@tutanota.com>
2023-02-10 18:27:16 +0200

    finish atom file parsing

--- src/app.zig
+++ src/app.zig
@@ -281,13 +281,6 @@ const FeedAndItems = struct {
     items: []FeedItem,
 };
 
-// feed_id: usize = 0,
-// title: ?[]const u8 = null,
-// feed_url: []const u8,
-// page_url: ?[]const u8 = null,
-// updated_raw: ?[]const u8 = null,
-// updated_timestamp: ?i64 = null,
-
 const AtomParseState = enum {
     feed,
     entry,
@@ -330,23 +323,21 @@ fn constructTmpStr(tmp_str: *TmpStr, data: []const u8) void {
 
 const xml = @import("zig-xml");
 const max_title_len = 1024;
+const default_item_count = 10;
 const TmpStr = std.BoundedArray(u8, max_title_len);
-pub fn parseAtom(allocator: Allocator, content: []const u8, url: []const u8) !void {
-    var tmp_str = try TmpStr.init(0);
-    var tmp_entries = try std.BoundedArray(FeedItem, 10).init(0);
-    var entries = try std.ArrayList(FeedItem).initCapacity(allocator, 10);
+pub fn parseAtom(allocator: Allocator, content: []const u8) !FeedAndItems {
+    var tmp_str = TmpStr.init(0) catch unreachable;
+    var tmp_entries = std.BoundedArray(FeedItem, 10).init(0) catch unreachable;
+    var entries = try std.ArrayList(FeedItem).initCapacity(allocator, default_item_count);
+    defer entries.deinit();
     var parser = xml.Parser.init(content);
-    var feed = Feed{
-        .feed_url = url,
-    };
+    var feed = Feed{ .feed_url = "" };
     var state: AtomParseState = .feed;
     var current_tag: ?AtomParseTag = null;
     var current_entry: ?*FeedItem = null;
     var link_href: ?[]const u8 = null;
     var link_rel: []const u8 = "alternate";
     while (parser.next()) |event| {
-        print("parent_tag: {?}\n", .{state});
-        print("  current_tag: {?}\n", .{current_tag});
         switch (event) {
             .open_tag => |tag| {
                 current_tag = AtomParseTag.fromString(tag);
@@ -384,8 +375,7 @@ pub fn parseAtom(allocator: Allocator, content: []const u8, url: []const u8) !vo
                                 if (mem.eql(u8, "alternate", link_rel)) {
                                     feed.page_url = href;
                                 } else if (mem.eql(u8, "self", link_rel)) {
-                                    // TODO: do I want to change feed_url?
-                                    // Already have it from fn args 'url'.
+                                    feed.feed_url = href;
                                 }
                             }
                             link_href = null;
@@ -403,17 +393,12 @@ pub fn parseAtom(allocator: Allocator, content: []const u8, url: []const u8) !vo
                                 if (link_href) |href| {
                                     if (mem.eql(u8, "alternate", link_rel)) {
                                         current_entry.?.link = href;
-                                    } else if (mem.eql(u8, "self", link_rel)) {
-                                        // TODO: do I want to change feed_url?
-                                        // Already have it from fn args 'url'.
                                     }
                                 }
                                 link_href = null;
                                 link_rel = "alternate";
                             },
-                            .id, .updated => {
-                                print("close entry\n", .{});
-                            },
+                            .id, .updated => {},
                         }
                         if (mem.eql(u8, "entry", tag)) {
                             current_entry = null;
@@ -440,7 +425,6 @@ pub fn parseAtom(allocator: Allocator, content: []const u8, url: []const u8) !vo
             .comment => {},
             .processing_instruction => {},
             .character_data => |data| {
-                print("character_data: |{s}|\n", .{data});
                 if (current_tag == null) {
                     continue;
                 }
@@ -468,20 +452,11 @@ pub fn parseAtom(allocator: Allocator, content: []const u8, url: []const u8) !vo
     if (tmp_entries.len > 0) {
         try entries.appendSlice(tmp_entries.slice());
     }
-    print("==== Feed ====\n", .{});
-    print("  title: {?s}\n", .{feed.title});
-    print("  feed_url: {?s}\n", .{feed.feed_url});
-    print("  page_url: {?s}\n", .{feed.page_url});
-    print("  updated_raw: {?s}\n", .{feed.updated_raw});
 
-    print("  title: {?s}\n", .{entries.items[0].title});
-    for (entries.items) |entry| {
-        print("==== Entry ====\n", .{});
-        print("  title: {s}\n", .{entry.title});
-        print("  id: {?s}\n", .{entry.id});
-        print("  link: {?s}\n", .{entry.link});
-        print("  updated_raw: {?s}\n", .{entry.updated_raw});
-    }
+    return .{
+        .feed = feed,
+        .items = try entries.toOwnedSlice(),
+    };
 }
 
 test "parseAtom" {
@@ -489,5 +464,27 @@ test "parseAtom" {
     defer arena.deinit();
 
     const content = @embedFile("atom.atom");
-    try parseAtom(arena.allocator(), content, "http://localhost/valid_url");
+    const result = try parseAtom(arena.allocator(), content);
+    const expect_feed = Feed{
+        .title = "Example Feed",
+        .feed_url = "http://example.org/feed/",
+        .page_url = "http://example.org/",
+        .updated_raw = "2012-12-13T18:30:02Z",
+    };
+    try std.testing.expectEqualDeep(expect_feed, result.feed);
+    var expect_items = [_]FeedItem{ .{
+        .title = "Atom-Powered Robots Run Amok",
+        .link = "http://example.org/2003/12/13/atom03",
+        .id = "urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a",
+        .updated_raw = "2008-11-13T18:30:02Z",
+    }, .{
+        .title = "Entry one's 1",
+        .link = "http://example.org/2008/12/13/entry-1",
+        .id = "urn:uuid:2225c695-dfb8-5ebb-baaa-90da344efa6a",
+        .updated_raw = "2005-12-13T18:30:02Z",
+    } };
+    // 'start' is a runtime value. Need value to be runtime to coerce array
+    // into a slice.
+    var start: usize = 0;
+    try std.testing.expectEqualDeep(expect_items[start..], result.items);
 }
