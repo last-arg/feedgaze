de4ab44b87834128d7367792177ecb18bc77a606
last-arg <last_arg@tutanota.com>
2023-02-16 19:15:42 +0200

    parse Rss datetime timezone

--- src/feed_types.zig
+++ src/feed_types.zig
@@ -22,15 +22,89 @@ pub const Feed = struct {
             self.feed_url = fallback_url.?;
         }
         _ = Uri.parse(self.feed_url) catch return Error.InvalidUri;
-        var timestamp: ?i64 = null;
         if (self.updated_raw) |date| {
+            // Can have two different date types
+            // Rss - Sat, 07 Sep 2002 00:00:01 GMT
+            //   len: 27 or 29
+            //   Year can be expressed with 2 or 4 characters
+            //   https://www.w3.org/Protocols/rfc822/#z28
+            // Atom - 2003-12-13T18:30:02Z
+            //   len: min - 20, max 28
+            //   https://www.rfc-editor.org/rfc/rfc4287#section-3.3
             // TODO: validate date string
             if (date.len > 0) {
-                timestamp = @as(i64, 22);
+                self.updated_timestamp = @as(i64, 22);
             }
         }
     }
 };
+
+const RssTimezone = enum {
+    UT,
+    GMT,
+    EST,
+    EDT,
+    CST,
+    CDT,
+    MST,
+    MDT,
+    PST,
+    PDT,
+    A,
+    M,
+    N,
+    Y,
+
+    pub fn toMinutes(raw: []const u8) !i16 {
+        // Timezone is made of letters
+        if (std.meta.stringToEnum(@This(), raw)) |tz| {
+            const result: i8 = switch (tz) {
+                .UT, .GMT => 0,
+                .EST => -5,
+                .EDT => -4,
+                .CST => -6,
+                .CDT => -5,
+                .MST => -7,
+                .MDT => -6,
+                .PST => -8,
+                .PDT => -7,
+                .A => -1,
+                .M => -12,
+                .N => 1,
+                .Y => 12,
+            };
+            return result * 60;
+        }
+
+        // Timezone is made of numbers
+        const first = raw[0];
+        if (raw.len == 5 and first != '+' and first != '-') {
+            return error.InvalidFormat;
+        }
+        const hour = std.fmt.parseInt(i16, raw[1..3], 10) catch return error.InvalidUri;
+        const min = std.fmt.parseUnsigned(i16, raw[3..5], 10) catch return error.InvalidUri;
+        // This is based on '+' and '-' ascii numeric values
+        const sign = -1 * (@as(i16, first) - 44);
+        return sign * ((hour * 60) + min);
+    }
+};
+
+fn parseRssDateTime(str: []const u8) !void {
+    _ = str;
+}
+
+test "parseRssDateTime" {
+    std.debug.print("{d} {d}\n", .{ '+', '-' });
+    // RssTimezone
+    {
+        const v1 = try RssTimezone.toMinutes("GMT");
+        const v2 = try RssTimezone.toMinutes("+0300");
+        const v3 = try RssTimezone.toMinutes("-0730");
+        std.debug.print("{d} {d} {d}\n", .{ v1, v2, v3 });
+    }
+    _ = try parseRssDateTime("Sat, 07 Sep 2002 00:00:01 GMT");
+    _ = try parseRssDateTime("Sat, 07 Sep 02 00:00:01 GMT");
+}
 
 pub const FeedItem = struct {
     feed_id: usize = 0,
