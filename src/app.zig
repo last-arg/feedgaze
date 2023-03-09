bd4d5f2971abeab2d03f8da16549dafb767bcdd6
last-arg <last_arg@tutanota.com>
2023-03-09 20:41:45 +0200

    add help messages for subcommands

--- src/app.zig
+++ src/app.zig
@@ -53,18 +53,15 @@ pub fn Cli(comptime Out: anytype) type {
         const Self = @This();
 
         pub fn run(self: *Self) !void {
-            // TODO?: use parseWithVerb?
-            // Is better for testing purposes
             var options = try args_parser.parseWithVerbForCurrentProcess(CliGlobal, CliVerb, self.allocator, .print);
             defer options.deinit();
 
             if (options.options.help) {
-                try self.printHelp();
+                try self.printHelp(options.verb);
                 return;
             }
 
             const verb = options.verb orelse {
-                try self.printHelp();
                 return;
             };
 
@@ -102,9 +99,9 @@ pub fn Cli(comptime Out: anytype) type {
             }
         }
 
-        fn printHelp(self: Self) !void {
+        fn printHelp(self: Self, verb: ?CliVerb) !void {
             // TODO: use stderr instead?
-            const output =
+            var output: []const u8 =
                 \\Usage: feedgaze [command] [options]
                 \\
                 \\Commands
@@ -121,6 +118,50 @@ pub fn Cli(comptime Out: anytype) type {
                 \\
                 \\
             ;
+
+            if (verb) |v| {
+                output = switch (v) {
+                    .add =>
+                    \\Usage: feedgaze add <url>
+                    \\
+                    \\  Add feed. Requires url to feed.
+                    \\
+                    \\Options:
+                    \\  -h, --help    Print this help and exit
+                    \\
+                    ,
+                    .remove =>
+                    \\Usage: feedgaze remove <search_term>
+                    \\
+                    \\  Remove feed. Requires search term. Search term will match page or feed url. 
+                    \\
+                    \\Options:
+                    \\  -h, --help    Print this help and exit
+                    \\
+                    ,
+                    .show =>
+                    \\Usage: feedgaze show [search_term]
+                    \\
+                    \\  Show feed(s). Optional search term. Search term will match page or feed url. 
+                    \\  Orders feeds by latest updated.
+                    \\
+                    \\Options:
+                    \\  -h, --help      Print this help and exit
+                    \\  -l, --limit     Set limit how many feeds to show
+                    \\  --item-limit    Set limit how many feed items to show
+                    ,
+                    .update =>
+                    \\Usage: feedgaze update [search_term]
+                    \\
+                    \\  Update feed(s). Optional search term. Search term will match page or feed url. 
+                    \\
+                    \\Options:
+                    \\  -h, --help    Print this help and exit
+                    \\  --force       Will force update all matched feeds
+                    ,
+                };
+            }
+
             _ = try self.out.write(output);
         }
 
@@ -240,6 +281,7 @@ pub fn Cli(comptime Out: anytype) type {
             }
 
             for (feeds) |feed| {
+                // TODO?: prompt user for to confirm deletion
                 try self.storage.?.deleteFeed(feed.feed_id);
                 std.log.info("Removed feed '{s}'", .{feed.feed_url});
             }
@@ -248,7 +290,7 @@ pub fn Cli(comptime Out: anytype) type {
         pub fn show(self: *Self, url: ?[]const u8, opts: ShowOptions) !void {
             var arena = std.heap.ArenaAllocator.init(self.allocator);
             defer arena.deinit();
-            const feeds = try self.storage.?.getLatestFeedsWithUrl(arena.allocator(), url orelse "", .{});
+            const feeds = try self.storage.?.getLatestFeedsWithUrl(arena.allocator(), url orelse "", opts);
 
             for (feeds) |feed| {
                 const title = feed.title orelse "<no title>";
