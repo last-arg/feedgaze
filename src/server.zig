5541d40cce8c031dfd3031cf07ba10bfcc5ee71b
last_arg <last_arg@tutanota.com>
2024-03-10 13:55:08 +0200

    human readable date

--- src/server.zig
+++ src/server.zig
@@ -1,7 +1,4 @@
-const config = @import("app_config.zig");
-const Datetime = @import("zig-datetime").datetime.Datetime;
-
-// valid date for <time>. Example: "2011-11-18T14:54:39.929Z"
+// Date for machine "2011-11-18T14:54:39.929Z". For <time datetime="...">.
 const date_fmt = "{[year]d}-{[month]d:0>2}-{[day]d:0>2}T{[hour]d:0>2}:{[minute]d:0>2}:{[second]d:0>2}.000Z";
 const date_len_max = std.fmt.comptimePrint(date_fmt, .{
     .year = 2222,
@@ -14,33 +11,64 @@ const date_len_max = std.fmt.comptimePrint(date_fmt, .{
 
 // For fast compiling and testing
 pub fn main() !void {
-    // try start();
-    try page_root_render();
+    std.debug.print("RUN MAIN\n", .{});
+    try start();
+    
+    // var general = std.heap.GeneralPurposeAllocator(.{}){};
+    // var arena = std.heap.ArenaAllocator.init(general.allocator());
+    // defer arena.deinit();
+    // const r = try page_root_render(&arena);
+    // std.debug.print("{s}\n", .{r});
 }
 
-fn page_root_render() !void {
+fn date_display(buf: []u8, a: i64, b: i64) ![]const u8 {
+    if (a < b) {
+        const dt = Datetime.fromSeconds(@floatFromInt(b));
+        // fallback date format: 01 Jan 2014
+        return try std.fmt.bufPrint(buf, "{d:0>2} {s} {d}", .{dt.date.day, dt.date.monthName()[0..3], dt.date.year});
+    }
+
+    const diff = a - b;
+    const mins = @divFloor(diff, 60);
+    const hours = @divFloor(mins, 60);
+    const days = @divFloor(hours, 24);
+    const months = @divFloor(days, 30);
+    const years = @divFloor(days, 365);
+
+    if (years > 0) {
+        return try std.fmt.bufPrint(buf, "{d}Y", .{years});
+    } else if (months > 0) {
+        return try std.fmt.bufPrint(buf, "{d}M", .{months});
+    } else if (days > 0) {
+        return try std.fmt.bufPrint(buf, "{d}d", .{days});
+    } else if (hours > 0) {
+        return try std.fmt.bufPrint(buf, "{d}h", .{hours});
+    } else if (mins == 0) {
+        return try std.fmt.bufPrint(buf, "0m", .{});
+    }
+
+    // mins > 0
+    return try std.fmt.bufPrint(buf, "{d}m", .{mins});
+}
+
+fn page_root_render(arena: *std.heap.ArenaAllocator) ![]const u8 {
     var db = try Storage.init("./tmp/feeds.db");
-    var general = std.heap.GeneralPurposeAllocator(.{}){};
-    var arena = std.heap.ArenaAllocator.init(general.allocator());
-    defer arena.deinit();
     const alloc = arena.allocator();
 
     const feeds = try db.feeds_all(alloc);
     const feeds_rendered = try feeds_render(&db, alloc, feeds);
-    _ = feeds_rendered;
-    // const writer = std.io.getStdOut().writer();
-    // try writer.writeAll(feeds_rendered);
-
+    return feeds_rendered;
 }
+
 
 const item_fmt = 
     \\<li>
     \\  <a href="{[link]s}">{[title]s}</a>
-    \\  <time datetime="{[date]s}">{[date]s}</time>
+    \\  <time datetime="{[date]s}">{[date_display]s}</time>
     \\</li>
 ;
 
-const item_no_fmt_len = std.fmt.comptimePrint(item_fmt, .{.link = "", .title = "", .date = ""}).len;
+const item_no_fmt_len = std.fmt.comptimePrint(item_fmt, .{.link = "", .title = "", .date = "", .date_display = ""}).len;
 // title + link = 128
 const average_item_len = item_no_fmt_len + date_len_max + 128;
 
@@ -57,6 +85,9 @@ fn feed_items_render(alloc: std.mem.Allocator, items: []FeedItemRender) ![]const
         capacity += if (item.link) |v| v.len else link_placeholder.len;
     }
 
+    const now_sec: i64 = @intFromFloat(Datetime.now().toSeconds());
+    var date_display_buf: [16]u8 = undefined;
+
     var date_buf: [date_len_max]u8 = undefined;
     var content = try std.ArrayList(u8).initCapacity(alloc, capacity);
     defer content.deinit();
@@ -65,6 +96,7 @@ fn feed_items_render(alloc: std.mem.Allocator, items: []FeedItemRender) ![]const
             .link = item.link orelse link_placeholder,
             .title = if (item.title.len > 0) item.title else title_placeholder,
             .date = timestampToString(&date_buf, item.updated_timestamp),
+            .date_display = if (item.updated_timestamp) |ts| try date_display(&date_display_buf, now_sec, ts) else "",
         }) catch unreachable;
     }
 
@@ -79,19 +111,18 @@ fn feeds_render(db: *Storage, alloc: std.mem.Allocator, feeds: []types.FeedRende
         \\<li>
         \\ <a href="{[page_url]s}">{[title]s}</a>
         \\ <a href="{[feed_url]s}">Feed url</a>
-        \\ <time datetime="{[date]s}">{[date]s}</time>
+        \\ <time datetime="{[date]s}">{[date_display]s}</time>
         \\ <ul class="feed-items-list">{[items]s}</ul>
         \\</li>
     ;
 
     const no_fmt_len = comptime blk: {
-        break :blk std.fmt.comptimePrint(li_html, .{.feed_url = "", .page_url = "", .title = "", .date = "", .items = ""}).len;
+        break :blk std.fmt.comptimePrint(li_html, .{.feed_url = "", .page_url = "", .title = "", .date = "", .items = "", .date_display = ""}).len;
     };
 
     const link_placeholder = "#";
     const title_placeholder = "[no-title]";
     var capacity = feeds.len * (no_fmt_len + date_len_max);
-
 
     for (feeds) |feed| {
         capacity += (feed.page_url orelse link_placeholder).len;
@@ -99,6 +130,9 @@ fn feeds_render(db: *Storage, alloc: std.mem.Allocator, feeds: []types.FeedRende
         capacity += if (feed.title.len > 0) feed.title.len else title_placeholder.len;
         capacity += config.max_items * average_item_len;
     }
+
+    const now_sec: i64 = @intFromFloat(Datetime.now().toSeconds());
+    var date_display_buf: [16]u8 = undefined;
 
     var date_buf: [date_len_max]u8 = undefined;
     var content = try std.ArrayList(u8).initCapacity(alloc, capacity);
@@ -112,6 +146,7 @@ fn feeds_render(db: *Storage, alloc: std.mem.Allocator, feeds: []types.FeedRende
             .title = if (feed.title.len > 0) feed.title else title_placeholder,
             .feed_url = feed.feed_url,
             .date = timestampToString(&date_buf, feed.updated_timestamp),
+            .date_display = if (feed.updated_timestamp) |ts| try date_display(&date_display_buf, now_sec, ts) else "",
             .items = items_rendered,
         });
     }
@@ -174,5 +209,6 @@ const types = @import("./feed_types.zig");
 const FeedItemRender = types.FeedItemRender;
 const storage = @import("./storage.zig");
 const Storage = storage.Storage;
-
+const config = @import("app_config.zig");
+const Datetime = @import("zig-datetime").datetime.Datetime;
 const index_html = @embedFile("./index.html");
