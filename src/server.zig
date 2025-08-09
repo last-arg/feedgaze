5bc89fab2c90e4901b049feb6b0323e93060f7ea
last-arg <last-arg@tutanota.com>
2025-08-09 08:35:01 +0300

    fix css order

--- src/server.zig
+++ src/server.zig
@@ -264,14 +264,14 @@ fn hash_static_file(comptime path: []const u8) [md5_len]u8 {
 
 const static_files = if (builtin.mode != .Debug) .{
     // .{ &hash_static_file("server/open-props-colors.css"), "open-props-colors.css" },
-    .{ &hash_static_file("server/style.css"), "style.css" },
     .{ &hash_static_file("server/kelp.css"), "kelp.css" },
+    .{ &hash_static_file("server/style.css"), "style.css" },
     .{ &hash_static_file("server/main.js"), "main.js" },
     .{ &hash_static_file("server/relative-time.js"), "relative-time.js" },
 } else .{
     // .{ "open-props-colors.css", "open-props-colors.css" },
-    .{ "style.css", "style.css" },
     .{ "kelp.css", "kelp.css" },
+    .{ "style.css", "style.css" },
     .{ "main.js", "main.js" },
     .{ "relative-time.js", "relative-time.js" },
     .{ "reload.js", "reload.js" },
