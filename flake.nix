73de8e204b5e032f8a1efce3153fd021f302556a
last-arg <last-arg@tutanota.com>
2025-09-22 16:54:32 +0300

    update zig version

--- flake.nix
+++ flake.nix
@@ -11,8 +11,8 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          # pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}."master-2025-06-23"; };
-          pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}."0.15.1"; };
+          pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}."master-2025-08-26"; };
+          # pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}."0.15.1"; };
           # pkgs = nixpkgs.legacyPackages.${system} // { zig = zig.packages.${system}.master; };
         in {
           devShell = pkgs.mkShell {
